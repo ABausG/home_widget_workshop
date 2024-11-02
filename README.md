ca606f59e12e5adeca1c8f8563a3ba0d9aea7fc9
Anton Borries <anton.borries@gmail.com>
2024-11-02 19:00:09 +0100

    chore: create project

---
+++ README.md
@@ -0,0 +1,4 @@
+# home_widget_workshop
+
+An example of adding HomeScreen Widgets to a Flutter App using home_widget
+
---
+++ android/app/src/main/kotlin/es/antonborri/home_widget_workshop/MainActivity.kt
@@ -0,0 +1,5 @@
+package es.antonborri.home_widget_workshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
