afbeb7624d0176a741da3513c46a34a94ef1bcc2
Anton Borries <anton.borries@gmail.com>
2024-11-07 19:27:23 +0100

    feat(ios): create actual intent definition

--- ios/Runner/HomeWidgetIntent.swift
+++ ios/Runner/HomeWidgetIntent.swift
@@ -5,4 +5,31 @@
 //  Created by Anton Borries on 07.11.24.
 //
 
+import AppIntents
+import Flutter
 import Foundation
+import home_widget
+
+@available(iOS 17, *)
+public struct BackgroundIntent: AppIntent {
+   static public var title: LocalizedStringResource = "HomeWidget Background Intent"
+
+   @Parameter(title: "Uri")
+   var url: URL?
+
+   @Parameter(title: "AppGroup")
+   var appGroup: String?
+
+   public init() {}
+
+   public init(url: URL?, appGroup: String?) {
+      self.url = url
+      self.appGroup = appGroup
+   }
+
+   public func perform() async throws -> some IntentResult {
+      await HomeWidgetBackgroundWorker.run(url: url, appGroup: appGroup!)
+
+      return .result()
+   }
+}
